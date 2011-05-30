c782279015d7e0f451347b987fc7f8e6c76d2a70
Christian Ludwig <cludwig@net.t-labs.tu-berlin.de>
2011-05-30 22:14:59 +0200

    pmap: Do not update the L4 task of a deactivated pmap
    If a pmap gets deactivated, it is going to go away anytime soon. So there are no
    updates necessary on the corresponding L4 task.

--- sys/arch/i386/l4/pmap.c
+++ sys/arch/i386/l4/pmap.c
@@ -1934,11 +1934,19 @@ pmap_switch(struct proc *o, struct proc *p)
 	s[sizeof(s)-1] = 0;
 	l4_debugger_set_object_name(pmap->task, s);
 #endif /* L4_DEBUG_REGISTER_NAMES */
+	TBUF_LOG3("pmap switch", o ? o->p_pid : 0, p->p_pid, 0);
 }
 
 void
 pmap_deactivate(struct proc *p)
 {
+#ifdef L4
+	struct pmap *pmap = p->p_vmspace->vm_map.pmap;
+
+	pmap_map_pdes(pmap);		/* locks pmap */
+	pmap->l4propagation = 0;
+	pmap_unmap_pdes(pmap);		/* unlocks pmap */
+#endif
 }
 
 /*
