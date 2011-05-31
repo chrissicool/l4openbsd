9b5db5c3d3c2095e0c376fa2ee92032d85789f4d
Christian Ludwig <cludwig@net.t-labs.tu-berlin.de>
2011-05-31 20:03:35 +0200

    pmap: Fix size of flexpage for l4_task_unmap
    When unmapping a page in a different task, the size of the flexpage is given in
    log2(size-in-bytes). Therefore, destroying a mapping with page size has a flexpage
    size of PGSHIFT. That also means, we cannot easily unmap more pages at once.
    
    Now that we got the size right, Fiasco does not unmap tons of pages for a single
    unmap operation anymore. That led to a bunch of problems. The unmap operation took
    quite some time. Additionally, we had to page in all the pages again. Both things
    were odd.

--- sys/arch/i386/l4/pmap.c
+++ sys/arch/i386/l4/pmap.c
@@ -352,7 +352,6 @@ caddr_t vmmap; /* XXX: used by mem.c... it should really uvm_map_reserve it */
  */
 
 void		 l4x_remove_pte(struct pmap *, vaddr_t, int);
-void		 l4x_remove_ptes(struct pmap *, vaddr_t, vaddr_t, int);
 struct pv_entry	*pmap_add_pvpage(struct pv_page *, boolean_t);
 struct vm_page	*pmap_alloc_ptp(struct pmap *, int, boolean_t, pt_entry_t);
 struct pv_entry	*pmap_alloc_pv(struct pmap *, int); /* see codes below */
@@ -523,49 +522,12 @@ l4x_remove_pte(struct pmap *pmap, vaddr_t va, int flush_rights)
 				va, pmap->pm_pdir);
 		L4XV_L(n);
 		tag = l4_task_unmap(pmap->task,
-				l4_fpage(va, NBPG, flush_rights),
+				l4_fpage(va, PGSHIFT, flush_rights),
 				L4_FP_ALL_SPACES);
 		L4XV_U(n);
 		if (l4_error(tag))
 			l4x_printf("Unmap error %ld\n",	l4_error(tag));
 	}
-}
-
-/*
- * Remove permission bits from all specified pages, starting from sva to eva.
- * VAs have to be aligned.
- * Note that eva points to the first page not to drop anymore.
- */
-void
-l4x_remove_ptes(struct pmap *pmap, vaddr_t sva, vaddr_t eva, int flush_rights)
-{
-	l4_msgtag_t tag;
-	L4XV_V(n);
-
-#ifdef DIAGNOSTIC
-	if (pmap == pmap_kernel()) {
-		printf("Removing kernel ptes for 0x%08lx - %08lx\n", sva, eva);
-		Debugger();
-		return;
-	}
-#endif
-
-	if (!pmap->l4propagation || l4_is_invalid_cap(pmap->task))
-		return;
-
-	pdb_printf("  %s: Removing %s%s%s bit(s) from (%#x--%#x) (PTD: %p)\n",
-			__func__,
-			(flush_rights & L4_FPAGE_RO) ? "R" : "",
-			(flush_rights & L4_FPAGE_W)  ? "W" : "",
-			(flush_rights & L4_FPAGE_X)  ? "X" : "",
-			sva, eva, pmap->pm_pdir);
-	L4XV_L(n);
-	tag = l4_task_unmap(pmap->task,
-			l4_fpage(sva, eva - sva, flush_rights),
-			L4_FP_ALL_SPACES);
-	L4XV_U(n);
-	if (l4_error(tag))
-		l4x_printf("Unmap error %ld\n",	l4_error(tag));
 }
 
 
@@ -2190,8 +2152,7 @@ pmap_remove_ptes(struct pmap *pmap, struct vm_page *ptp, vaddr_t ptpva,
 		/* atomically save the old PTE and zero it */
 		opte = i386_atomic_testset_ul(pte, 0);
 #ifdef L4
-		if ((pmap == pmap_kernel()) || !(flags == PMAP_REMOVE_ALL))
-			l4x_remove_pte(pmap, startva, L4_FPAGE_RWX);
+		l4x_remove_pte(pmap, startva, L4_FPAGE_RWX);
 #endif
 
 		if (opte & PG_W)
@@ -2269,13 +2230,6 @@ pmap_do_remove(struct pmap *pmap, vaddr_t sva, vaddr_t eva, int flags)
  	PMAP_MAP_TO_HEAD_LOCK();
 	pd = pmap_map_pdes(pmap);	/* locks pmap */
 
-
-#ifdef L4
-	/* speed hack: we are instructed to remove the whole range. */
-	if ((pmap != pmap_kernel()) && (flags == PMAP_REMOVE_ALL)) {
-		l4x_remove_ptes(pmap, sva, eva, L4_FPAGE_RWX);
-	}
-#endif
 
 	/*
 	 * Decide if we want to shoot the whole tlb or just the range.
@@ -2611,9 +2565,6 @@ pmap_write_protect(struct pmap *pmap, vaddr_t sva, vaddr_t eva,
 		type |= L4_FPAGE_RO;
 	if ((prot & VM_PROT_EXECUTE) == 0)
 		type |= L4_FPAGE_X;
-
-	if (pmap != pmap_kernel())
-		l4x_remove_ptes(pmap, sva, eva, type);
 #endif
 
 
@@ -2667,8 +2618,7 @@ pmap_write_protect(struct pmap *pmap, vaddr_t sva, vaddr_t eva,
 				    (~md_prot & opte) & PG_PROT);
 				i386_atomic_setbits_l(spte, md_prot);
 #ifdef L4
-				if (pmap == pmap_kernel())
-					l4x_remove_pte(pmap, va, type);
+				l4x_remove_pte(pmap, va, type);
 #endif
 			}
 		}
